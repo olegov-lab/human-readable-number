428849a33659611907ce52a8d859f9a7b3431d64
olegov-lab <olego8760@gmail.com>
2021-01-30 17:29:07 +0300

    test: done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
-}
+    if (number == 0) {
+        return 'zero';
+    }
+
+    let nHund = Math.floor(number/100);
+    let nTen = Math.floor((number - nHund * 100)/10);
+    let nOne = number - nHund*100 - nTen*10;
+
+    if (nTen == 1) {
+        ten = 0;
+        nOne = number - nHund * 100;
+     }
+
+    let nOneArray = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+                    'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+                    'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let nTenArray = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ',
+               'eighty ', 'ninety '];
+    let nHundArray = nHund ? nOneArray[nHund] + ' hundred ' : '';
+    return (nHundArray + nTenArray[nTen] + nOneArray[nOne]).trim();
+
+  }
+
+
+
